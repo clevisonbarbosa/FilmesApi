802752ead3b5420779fd5e79294bfae3bb18de96
Clévison barbosa <50533700+clevisonbarbosa@users.noreply.github.com>
2024-01-18 20:14:11 -0300

    first commit

---
+++ Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class CreateFilmeDto
+{
+    [Required(ErrorMessage = "O título do filme é obrigatório.")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O Gênero do filme é obrigatório.")]
+    [StringLength(50, ErrorMessage = "O tamanho do Gênero não deve exceder 50 caracteres.")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "A duração do filme é obrigatório.")]
+    [Range(70, 600, ErrorMessage = "A duração do filme deve ser de entre 70 a 600 minutos.")]
+    public int Duracao { get; set; }
+}
+
